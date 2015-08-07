e53d5347ef56149051a54d0cb711e22c26cd945a
H14me <h14me-2good4u@outlook.com>
2015-08-07 18:18:21 +0900

    3,4

---
+++ 3.py
@@ -0,0 +1,21 @@
+text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+text_split = text.split()
+text0 = text_split[0]
+text1 = text_split[1]
+text2 = text_split[2]
+text3 = text_split[3]
+text4 = text_split[4]
+text5 = text_split[5]
+text6 = text_split[6]
+text7 = text_split[7]
+text8 = text_split[8]
+text9 = text_split[9]
+text10 = text_split[10]
+text11 = text_split[11]
+text12 = text_split[12]
+text13 = text_split[13]
+text14 = text_split[14]
+
+
+print list(text0[0]+text1[0]+text2[0]+text3[0]+text4[0]+text5[0]+text6[0]+text7[0]+text8[0]+text9[0]+text10[0]+text11[0]+text12[0]+text13[0]+text14[0])
