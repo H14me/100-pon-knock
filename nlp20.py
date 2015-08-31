b774782089e45522c33cc677ebf5e5b0510903d8
H14me <h14me-2good4u@outlook.com>
2015-08-31 14:20:56 +0900

    update

--- nlp20.py
+++ nlp20.py
@@ -9,9 +9,6 @@ for line in f:
 	text = json.loads(line) 
 	if text["title"] == target_country:
 		print text["text"]
-		# ans = json.dumps(text, ensure_ascii=False)##json.dumpsで日本語で出なかったら、ensure_ascii使って日本語にする
-
-# for line in ans.split("\n"):
-# 	print line
+		
 
 f.close()
